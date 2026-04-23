28f7794b17ec36b1c7054119c19b06b2c9c7ed26
Abi Athmakuri <aathmakuri034@gmail.com>
2026-04-23 13:11:17 -0500

    Update: finished assignment

---
+++ index.js
@@ -0,0 +1,7 @@
+let input = require("readline-sync");
+
+let phrase = "Javascript rules and is better than python!!!"
+let userIndex = input.question("Give me an index value: ");
+
+console.log(phrase[userIndex]);
+
